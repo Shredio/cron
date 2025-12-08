ea685bd9c4d541dd1f44ea219c01bd61c718b6e1
MartkCz <martk494@gmail.com>
2025-12-09 00:44:50 +0100

    refactor: remove unused CompilerPassInterface and related logic from CronBundle

--- src/Symfony/CronBundle.php
+++ src/Symfony/CronBundle.php
@@ -2,17 +2,11 @@
 
 namespace Shredio\Cron\Symfony;
 
-use LogicException;
-use ReflectionClass;
-use Shredio\Cron\CronJobReflector;
-use Symfony\Component\Console\Attribute\AsCommand;
-use Symfony\Component\Console\Command\Command;
-use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 
-final class CronBundle extends AbstractBundle implements CompilerPassInterface
+final class CronBundle extends AbstractBundle
 {
 
 	/**
@@ -27,50 +21,6 @@ final class CronBundle extends AbstractBundle implements CompilerPassInterface
 			->autowire();
 		$services->set(ConsoleMemorySubscriber::class)
 			->tag('kernel.event_subscriber');
-	}
-
-	public function process(ContainerBuilder $container): void
-	{
-		$memoryLimits = [];
-
-		foreach ($container->findTaggedServiceIds('console.command') as $id => $tags) {
-			$className = $container->getDefinition($id)->getClass();
-			if ($className === null) {
-				continue;
-			}
-			if (!class_exists($className)) {
-				continue;
-			}
-
-			$reflection = new ReflectionClass($className);
-			if ($reflection->isSubclassOf(Command::class)) {
-				continue;
-			}
-
-			$memoryLimit = CronJobReflector::extractMemoryLimit($reflection);
-			if ($memoryLimit === null) {
-				continue;
-			}
-
-			$commandName = CronJobReflector::extractCommand($reflection);
-			if ($commandName === null) {
-				throw new LogicException(sprintf(
-					'Class "%s" is tagged as "console.command" but does not have the "%s" attribute.',
-					$className,
-					AsCommand::class
-				));
-			}
-
-			$memoryLimits[$commandName] = $memoryLimit->toArray();
-		}
-
-		$container->findDefinition('cron.memory_setter')
-			->setArgument(0, $memoryLimits);
-	}
-
-	public function build(ContainerBuilder $container): void
-	{
-		$container->addCompilerPass($this);
 	}
 
 }
